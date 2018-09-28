1dfefb504de476e6ea1eb92d768876f4e6cfcd96
CHN <1170951549@qq.com>
2018-09-28 10:09:34 +0800

    928

--- my-haonan/app.js
+++ my-haonan/app.js
@@ -34,10 +34,10 @@ app.use(express.static('dist'));
 // });
 app.all('*', function(req, res, next) {
   console.log(req.method);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Headers', 'Content-type');
-  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH");
-  res.header('Access-Control-Max-Age',1728000);//预请求缓存20天
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.header('Access-Control-Allow-Credentials', true);//预请求缓存20天
   next();
 });
 
