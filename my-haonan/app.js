29526ee01721c5657dde8d215dee9184992544ab
CHN <1170951549@qq.com>
2018-09-28 11:06:32 +0800

    928

--- my-haonan/app.js
+++ my-haonan/app.js
@@ -36,9 +36,14 @@ app.all('*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  res.header("X-Powered-By",' 3.2.1')
-  if(req.method=="OPTIONS") res.send(200);/*让options请求快速返回*/
-  else  next();
+  if(req.method=="OPTIONS"){
+    res.send(200);/*让options请求快速返回*/
+    console.log(111111);
+  }
+  else {
+    next();
+    console.log(222);
+  }
 });
 
 app.post('/upload',(req,res)=>{
