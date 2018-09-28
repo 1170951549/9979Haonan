56b4b3fea09efb9455a56992783f16ad795c6ff8
CHN <1170951549@qq.com>
2018-09-28 09:28:10 +0800

    928

--- my-haonan/app.js
+++ my-haonan/app.js
@@ -43,6 +43,18 @@ app.post('/upload',(req,res)=>{
     }
   });
 });
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+
+  if (req.method == 'OPTIONS') {
+    res.send(200); /让options请求快速返回/
+  }
+  else {
+    next();
+  }
+});
 
 var server = app.listen(PORT,"0.0.0.0", function () {
   console.log('已启动', PORT);
