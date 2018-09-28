69abbb20c1051271a3d211828791d08db1862edb
CHN <1170951549@qq.com>
2018-09-28 09:35:42 +0800

    928

--- my-haonan/app.js
+++ my-haonan/app.js
@@ -19,6 +19,18 @@ app.use(express.static('dist'));
 // app.engine( '.html', require( 'ejs' ).__express );
 // app.use(router);
 
+app.all('*',function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+  if (req.method == 'OPTIONS') {
+    res.send(200); /让options请求快速返回/
+  }
+  else {
+    next();
+  }
+});
+
 app.post('/upload',(req,res)=>{
   console.log(req.body.base64);
   //res.send("0");
@@ -42,18 +54,6 @@ app.post('/upload',(req,res)=>{
       res.send(imgPath);
     }
   });
-});
-app.all('*',function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
-  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-
-  if (req.method == 'OPTIONS') {
-    res.send(200); /让options请求快速返回/
-  }
-  else {
-    next();
-  }
 });
 
 var server = app.listen(PORT,"0.0.0.0", function () {
