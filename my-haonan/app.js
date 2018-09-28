04d6319b637b4c7e3327e8863d0a7c287af7544d
CHN <1170951549@qq.com>
2018-09-28 14:24:03 +0800

    928

--- my-haonan/app.js
+++ my-haonan/app.js
@@ -9,69 +9,67 @@ const PORT = 3010;
 
 const cors = require('cors');
 const uuid = require('node-uuid');
-
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false ,limit: 2 * 1024 * 1024 * 1024 * 1024}));
+app.use(bodyParser.urlencoded({extended: false, limit: 2 * 1024 * 1024 * 1024 * 1024}));
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,username");
+  //res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  //res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  //console.log( req.method);
+  next();
+});
 app.use(express.static('dist'));
 // app.set("views", path.join(__dirname, "dist"));
 // app.set("view engine", "html");
 // app.engine( '.html', require( 'ejs' ).__express );
 // app.use(router);
 
-// app.all('*',function (req, res, next) {
+
+
+// app.all('*', function (req, res, next) {
 //   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//   res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-//   res.header("X-Powered-By",' 3.2.1');
-//   res.header("Content-Type", "application/json;charset=utf-8");
-//   if (req.method == 'OPTIONS') {
-//     res.send(200); /让options请求快速返回/
+//   res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+//   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+//   if (req.method == "OPTIONS") {
+//     res.send(200);
+//     /*让options请求快速返回*/
+//     console.log(111111);
 //   }
 //   else {
 //     next();
+//     console.log(222);
 //   }
 // });
-app.all('*', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
-  res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-  if(req.method=="OPTIONS"){
-    res.send(200);/*让options请求快速返回*/
-    console.log(111111);
-  }
-  else {
-    next();
-    console.log(222);
-  }
-});
 
-app.post('/upload',(req,res)=>{
+app.post('/upload', (req, res) => {
   console.log(req.body.base64);
   //res.send("0");
   //console.log(uuid.v4());
-  var imgPath=uuid.v4();
-  var tpye=".unknow";
-  if(req.body.base64.indexOf("image/png")>0)
-    tpye=".png";
-  if(req.body.base64.indexOf("image/jpg")>0)
-    tpye=".jpg";
-  if(req.body.base64.indexOf("image/jpeg")>0)
-    tpye=".jpeg";
-  imgPath+=tpye;
+  var imgPath = uuid.v4();
+  var tpye = ".unknow";
+  if (req.body.base64.indexOf("image/png") > 0)
+    tpye = ".png";
+  if (req.body.base64.indexOf("image/jpg") > 0)
+    tpye = ".jpg";
+  if (req.body.base64.indexOf("image/jpeg") > 0)
+    tpye = ".jpeg";
+  imgPath += tpye;
   var base64Data = req.body.base64.replace(/^data:([A-Za-z-+/]+);base64,/, '');
   var dataBuffer = new Buffer(base64Data, 'base64');
-  var savePath=__dirname + '/dist/img/' + imgPath;
-  fs.writeFile(savePath, dataBuffer, function(err) {
-    if(err){
+  var savePath = __dirname + '/dist/img/' + imgPath;
+  fs.writeFile(savePath, dataBuffer, function (err) {
+    if (err) {
       res.send("0");
-    }else{
+    } else {
       res.send(imgPath);
     }
   });
 });
 
-var server = app.listen(PORT,"0.0.0.0", function () {
+var server = app.listen(PORT, "0.0.0.0", function () {
   console.log('已启动', PORT);
 });
 
