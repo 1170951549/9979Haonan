64caeb44d00176b9e90d30e398d50a491dce1e59
CHN <1170951549@qq.com>
2018-10-29 17:49:57 +0800

    1029

--- my-haonan/src/assets/plick.js
+++
@@ -1,7 +0,0 @@
-(function(){
-  $(".Hui-aside dl").on("click",function(){
-    console.log($(this))
-    $(".Hui-aside dl dd").hide();
-    $(".Hui-aside dl").eq($(".Hui-aside dl dd").index($(this))).show()
-  })
-})
